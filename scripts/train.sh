957176451d1748eed1418fbd01e31abd9a8067df
xuanmanh <90127093+xmak9102@users.noreply.github.com>
2023-06-27 11:13:51 +0700

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,12 +4,12 @@ base_dir="."
 data_dir="${base_dir}/gec_private_train_data"
 detect_vocab_path="./data/vocabulary/d_tags.txt"
 correct_vocab_path="./data/vocabulary/labels.txt"
-train_path="${data_dir}/train-stage1.edits"
-valid_path="${data_dir}/test-stage1.edits"
+train_path="${data_dir}/train-stage2.edits"
+valid_path="${data_dir}/test-stage2.edits"
 config_path="configs/ds_config_basic.json"
 timestamp=`date "+%Y%0m%0d_%T"`
 save_dir="../ckpts/ckpt_$timestamp"
-pretrained_transformer_path="xlm-roberta-large"
+pretrained_transformer_path="xlm-roberta-base"
 mkdir -p $save_dir
 cp $0 $save_dir
 cp $config_path $save_dir
@@ -22,7 +22,7 @@ run_cmd="deepspeed --include localhost:0 --master_port 42997 train.py \
     --config_path $config_path \
     --num_epochs 20 \
     --max_len 256 \
-    --train_batch_size 32 \
+    --train_batch_size 16 \
     --accumulation_size 1 \
     --valid_batch_size 256 \
     --cold_step_count 0 \
